8da0e06c8cf93ccc43cbc60e077005dd0bc147c4
oleanto <olegantonenko100@gmail.com>
2025-03-20 17:37:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,26 @@
 def copy_file(command: str) -> None:
-    elements = command.split()
-    if len(elements) != 3 and elements[0] != "cp":
+
+    if not command.strip():
         return
 
-    _, source_file, destination_file = elements
+    command_elements = command.split()
+
+    if len(command_elements) != 3 or command_elements[0] != "cp":
+        return
+
+    _, source_file, destination_file = command_elements
 
     if source_file == destination_file:
         return
 
     try:
-        with (open(source_file, "r") as
-              file_in, open(destination_file, "w") as file_out):
+        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
             file_out.write(file_in.read())
     except FileNotFoundError:
         print(f"Error: The file {source_file} does not exist.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt new_file.txt")
